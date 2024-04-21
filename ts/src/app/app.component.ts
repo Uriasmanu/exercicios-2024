f530a410eaccb0a44d1c4439af5ba42d2232cf56
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-04-20 23:31:50 -0300

    .

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+  textExpanded: boolean = false;
+  buttonText: string = 'Ver mais';
+  textBoxHeight: string = '200px';
+
+  toggleText() {
+    this.textExpanded = !this.textExpanded;
+    if (this.textExpanded) {
+      this.textBoxHeight = 'auto';
+      this.buttonText = '';
+    } else {
+      this.textBoxHeight = '200px';
+      this.buttonText = 'Ver mais';
+    }
+  }
 }
+
+
