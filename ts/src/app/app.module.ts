322db7ffa58b55c4cb6060c1ef23fc3b702c04de
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-04-20 23:34:50 -0300

    .

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DiscussaoComponent } from './discussao/discussao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiscussaoComponent
   ],
   imports: [
     BrowserModule
---
+++ ts/src/app/discussao/discussao.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discussao',
+  templateUrl: './discussao.component.html',
+  styleUrls: ['./discussao.component.scss']
+})
+export class DiscussaoComponent {
+
+  showTopicForm: boolean = false;
+  showTopcoResp: boolean = false;
+  topicSubject: string = '';
+  topicContent: string = '';
+
+  toggleTopicForm() {
+    this.showTopicForm = !this.showTopicForm;
+    // Resetar a exibição de topco-Resp quando o formulário é alternado
+    this.showTopcoResp = false;
+  }
+
+  updateSubject(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.topicSubject = inputElement.value;
+  }
+
+  updateContent(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.topicContent = inputElement.value;
+  }
+
+  submitTopic() {
+    // Aqui você pode adicionar lógica para lidar com o envio do tópico
+    console.log('Assunto:', this.topicSubject);
+    console.log('Conteúdo:', this.topicContent);
+
+    // Após o envio, exibir topco-Resp e iniciar temporizador para ocultá-lo após 5 segundos
+    this.showTopcoResp = true;
+    setTimeout(() => {
+      this.showTopcoResp = false;
+    }, 3000);
+
+    // Após o envio, ocultar o formulário novamente
+    this.showTopicForm = false;
+  }
+}
